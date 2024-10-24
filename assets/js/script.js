c19a0da12e640cd972bcdf1b739ab56b6b57ce0c
Nathalie von Heyl <nath.vonheyl@gmail.com>
2024-10-24 19:35:55 +0000

    fix division game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -88,7 +88,7 @@ function calculateCorrectAnswer() {
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
     } else if (operator === "/") {
-        return [Math.floor(operand1 / operand2), "division"];
+        return [operand1 / operand2, "division"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`; 
